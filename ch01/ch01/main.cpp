93f17e2c45357c27163aa480cdd1dcac384d4b9c
hugo <werwer748@naver.com>
2023-08-23 01:05:14 +0900

    1학년 2학기 cpp첫수업

---
+++ ch01/ch01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ch01
+//
+//  Created by 강준기 on 2023/08/23.
+//
+
+#include <iostream>
+
+int main() {
+    // 표준 출력 스트림으로 문장을 출력함
+    std::cout << "나의 첫 번째 c++ 프로그램" << std::endl;
+    return 0;
+}
